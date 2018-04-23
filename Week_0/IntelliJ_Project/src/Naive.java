4e63be6f8eb75c7b2c04f29725815f6bc21f4bc5
Vincent Roller <v.roller.uni@gmail.com>
2018-04-23 16:33:37 +0200

    Interface to Abstract Class

--- Week_0/IntelliJ_Project/src/Naive.java
+++ Week_0/IntelliJ_Project/src/Naive.java
@@ -3,24 +3,19 @@ import java.util.List;
 
 public class Naive extends SequenceAlgorithm {
 
-
     @Override
     public List<Integer[]> AMSS(Integer[] sequence) {
         List<Integer[]> results = new ArrayList<>();
-
         int n = sequence.length;
         int maxscore = Integer.MIN_VALUE;
         int l = 0;
         int r = 0;
-
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-
                 int s=0;
                 for (int k = i; k < j+1; k++) {
                     s+=sequence[k];
                 }
-
                 if (s >= maxscore){
                     l=i;
                     r=j;
@@ -30,15 +25,8 @@ public class Naive extends SequenceAlgorithm {
                     }
                     results.add(new Integer[]{l,r,s});
                 }
-
-
-
-
             }
-
         }
-
-
         return results;
     }
 }
--- Week_0/IntelliJ_Project/src/SequenceAlgorithm.java
+++ Week_0/IntelliJ_Project/src/SequenceAlgorithm.java
@@ -2,16 +2,6 @@ import java.util.List;
 
 public abstract class SequenceAlgorithm {
 
-    /**
-     * Die Methode soll jeweils für die übergebene Sequenz, die Resultate als int[] Liste ausgeben.
-     * Wenn das Flag findShortest true ist dann soll die Methode das SMSS finden ansonsten alle MSS.
-     *
-     * @param sequence Die analysierte Sequenz als Integer[], wird von der main Methode übergeben.
-     * @return Die Liste die die int[] ethält, bei der variante findShortest==true ist dann nur ein Element in der Liste.
-     *         Jedes Element der Liste ist ein int[] der Länge 3 wobei das ertse Array-Element den Anfangsindex, das
-     *         zweite den Endindex und das dritte den Score darstellt.
-     */
-
     public abstract List<Integer[]> AMSS(Integer[] sequence);
 
     public List<Integer[]> SMSS(Integer[] sequence){
